c25bcbbb004a60c40e2f068df296115b19e4b87f
Brett Larson <larson.brett.s@gmail.com>
2025-03-02 18:59:10 -0500

    Create main.py

---
+++ nerdgraph/main.py
@@ -0,0 +1,20 @@
+from utils import AppLogger
+from datetime import timedelta
+from api.rate_limiter import RateLimiter
+from api.nerdgraph_client import NerdGraphClient
+
+# Initialize the logger
+logger = AppLogger.get_instance(__name__).get_logger()
+
+def main():
+
+    logger.info("**********Starting the NerdGraph API Application**********")
+    rate_limiter = RateLimiter(1000, timedelta(minutes=1))
+    nerdgraph_client = NerdGraphClient()
+
+
+
+    logger.info("**********Ending the NerdGraph API Application**********")
+    
+if __name__ == '__main__':
+    main()
